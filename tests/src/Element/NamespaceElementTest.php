18ad3b3f503c385af05661a5a1c2fa8673ab07b8
tanaka <step.by.step524@gmail.com>
2017-11-14 13:10:37 +0900

    [tests] add namespace test

--- tests/src/Element/NamespaceElementTest.php
+++ tests/src/Element/NamespaceElementTest.php
@@ -3,6 +3,7 @@ namespace PruneMazui\ZephirIdeHelper\Element\Tests;
 
 use PruneMazui\ZephirIdeHelper\Element\NamespaceElement;
 use PHPUnit\Framework\TestCase;
+use PruneMazui\ZephirIdeHelper\DefinitionException;
 
 class NamespaceElementTest extends TestCase
 {
@@ -18,5 +19,29 @@ class NamespaceElementTest extends TestCase
         assertEquals($params['name'], $namespace->getName());
         assertFalse($namespace->hasClass());
         assertFalse($namespace->hasUse());
+
+        // --------------------------------------
+
+        $namespace = NamespaceElement::factory([
+            'type' => 'namespace',
+            'name' => '',
+        ]);
+
+        assertEmpty($namespace->getName());
+        assertFalse($namespace->hasClass());
+        assertFalse($namespace->hasUse());
+    }
+
+    public function testFailure()
+    {
+        try {
+            NamespaceElement::factory([
+                'type' => 'not_namespace',
+                'name' => 'fuga',
+            ]);
+            $this->fail('excepted throw by miss match type.');
+        } catch (DefinitionException $ex) {
+            $this->addToAssertionCount(1);
+        }
     }
 }
